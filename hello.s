41187dff3eb92c7148a2f98a987b5ba810503483
Edouard <ed_srv@proton.me>
2026-05-12 21:47:50 +0200

    hello world almost understood

--- hello.s
+++ hello.s
@@ -1,13 +1,20 @@
-SECTION	.data
-msg	db	'Hello World!', 0Ah
+BITS 64
+default rel
 
-SECTION	.text
+section	.data
+msg: db "Hello world", 0x0A ; message + newline
+
+section	.text
 global	_start
 
 _start:
-	
-	mov	rdi, 1
-	mov	rsi, msg
-	mov	rdx, 13
-	mov	rax, 1
+	; write(1, msg, len)
+	mov	rax, 1	; sys_write
+	mov	rdi, 1	; fd = 1 stdout
+	lea	rsi, [rel msg]	; pointer to message
+	mov	rdx, 12	; 12 bytes to write to stdout
+	syscall
+
+	mov	rax, 60
+	xor	rdi, rdi
 	syscall
